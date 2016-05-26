6a6ec97a26dbbf698187bf47658fd22a841e69ec
user <495871401>
2016-05-27 00:10:23 +0800

    Initial Commit

---
+++ 设计模式/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  设计模式
+//
+//  Created by user on 16/5/27.
+//  Copyright © 2016年 mredrock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
